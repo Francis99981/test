d168ec061164a7dc8bc371116fd46b397140645d
Francis88891 <frankibuba50@gmail.com>
2023-05-30 18:27:24 +0300

    first

--- Kib/Kibshop/models.py
+++ Kib/Kibshop/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-Class Shopping List
+
+class Shopping(models.Model):
+    name= models.CharField(max_length=200, null=True)
+    shopping = models.TextField()
+
+    def __str__(self):
+        return self.shopping
+ 
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class OrderModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    items = models.ManyToManyField('Shopping', 'order',blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
+    phone_number = models.IntegerField(max_length=12, null=True)
+    location = models.CharField(max_length=200, null=True)
+    is_paid = models.BooleanField(default=False)
+    is_shipped = models. BooleanField(default=False)
+
+    def __str__(self):
+        return f'Order: {self.created_on.strftime("%b %d %I %M %P")}'
+
--- Kib/Kibshop/views.py
+++ Kib/Kibshop/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django .views import View
+from .models import *
 
-# Create your views here.
+
+class Index (View):
+    def get (self, request, *args, **kwargs):
+        return render(request, 'index.html')
+
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        list1 = Shopping.objects.filter(category__name__contains='regular')
+        list2 = Shopping.objects.filter(category__name__contains='urgent')
+
+        context ={
+            'list1': list1,
+            'list2': list2,
+        }
+        return render(request, 'store.html',)
+
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        location = request.POST.get('location')
+        phone_number = request.POST.get('phone_number')
+
+        order_items = {
+            'items':[]
+        }
+        items = request.POST.getlist('items[]')
+        for item in items:
+            shopping = Shopping.objects.get(pk__contains=int(item))
+            item_data ={
+                'id':shopping.pk,
+                'name':shopping.name,
+                'price':shopping.price
+            }
+            order_items['item'].append(item_data)
+            price = 0
+            item_ids = []
+
+        for item in order_items['items']:
+
+            item_ids.append(item['id'])
+
+        order = OrderModel.objects.create(
+            name=name,
+            email=email,
+            location=location,
+            phone_number=phone_number
+        )
+        context = {
+            'items': order_items['items'],
+
+        }
+        return redirect('checkout', pk=order.pk,)
+
